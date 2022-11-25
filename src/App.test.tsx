5316d1c3d86e9c288e21f8e5dc91f82c69ea51e4
Jan Gregor <32632196+gregorjan@users.noreply.github.com>
2022-11-25 19:13:56 +0100

    added vitest (#4)

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+import App from './App'
+
+describe('App', () => {
+  it('renders', () => {
+    const screen = render(<App />)
+    //* baseElement is a wrapper around App. To access App we need to grab first child.
+    const app = screen.baseElement.children[0]
+    console.log(app.hasChildNodes())
+    expect(app.hasChildNodes()).toBe(true)
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setupTests.ts',
+    outputFile: './testResults.json',
+  },
 })
