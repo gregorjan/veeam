c9429ac738182e3208458121c43ae9bdf41260d1
Jan Gregor <32632196+gregorjan@users.noreply.github.com>
2022-11-28 00:55:16 +0100

    feat | Button (#13)
    * feat | Button
    
    * Fixed build and cleanup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,41 @@
 import { ComponentProps } from 'react'
 import { styled } from '@linaria/react'
+import { borderRadius, colors, rem, spacers } from '@/styles'
 
-interface ButtonProps extends ComponentProps<'button'> {}
+interface StyledProps {
+  variant?: 'primary' | 'secondary'
+}
+interface ButtonProps extends ComponentProps<'button'>, StyledProps {}
 
-const StyledButton = styled.button``
+const StyledButton = styled.button<StyledProps>`
+  appearance: none;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  cursor: pointer;
+  text-transform: uppercase;
+  text-decoration: none;
+  white-space: nowrap;
+  font-weight: 500;
+  border-radius: ${borderRadius.sm};
+  background-color: ${colors.transparent};
+  padding: ${rem(spacers.spacer3)} ${rem(spacers.spacer4)};
+  color: ${({ variant }) => (variant === 'primary' ? colors.white : colors.black)};
+  background: ${({ variant }) => (variant === 'primary' ? colors.black : colors.transparent)};
+  border: 1px solid ${({ variant }) => (variant === 'primary' ? colors.transparent : colors.black)};
+  &:hover {
+    background-color: ${colors.grey};
+  }
 
-export const Button = ({ children, type = 'button', ...props }: ButtonProps) => {
+  &:active {
+    background-color: ${colors.grey};
+  }
+`
+
+export const Button = ({ children, type = 'button', variant = 'primary', ...props }: ButtonProps) => {
   return (
-    <StyledButton type={type} {...props}>
+    <StyledButton type={type} variant={variant} {...props}>
       {children}
     </StyledButton>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import linaria from '@linaria/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
     linaria({
       sourceMap: process.env.NODE_ENV !== 'production',
+      babelOptions: {
+        presets: ['@babel/preset-typescript'],
+      },
     }),
   ],
   test: {
